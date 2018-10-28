f776ae9079b38e097ce2dd2e8d1061ed9ab9a3af
Amirhossein <amirhossein1398021@gmail.com>
2018-10-28 22:10:28 +0330

    connected to api

--- src/core/router/appRouter.js
+++ src/core/router/appRouter.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Router,Route , Switch } from 'react-router-dom';
 import LoginForm from '../../views/componets/login&signup/loginForm'
+import Signup_Form from '../../views/componets/login&signup/signup_form'
 import history from '../history/history'
 
 const AppRouter = () => (
     <Router history={history}>
         <Switch>
             <Route path="/" component={LoginForm} exact={true}/>
+            <Route path="/signUp" component={Signup_Form} />
         </Switch>
     </Router>
     
