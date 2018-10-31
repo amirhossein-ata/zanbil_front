1aa1cc5aa57a6ad89897818cbb8d148327887d3d
Amirhossein <amirhossein1398021@gmail.com>
2018-10-31 16:04:58 +0330

    time range slider added

--- src/views/pages/main_page.js
+++ src/views/pages/main_page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../componets/navbar/navbar'
 import BusinessPage from './business_page'
 import { Grid } from 'semantic-ui-react';
+import TimeRangeSlider from '../componets/time_slider/time_slider'
 class Main_page extends React.Component{
     render(){
         return(
@@ -13,6 +14,12 @@ class Main_page extends React.Component{
                     <Grid.Column computer={14}>
                         <BusinessPage/>
                     
+                    </Grid.Column>
+                </Grid>
+
+                <Grid centered>
+                    <Grid.Column computer={14}>
+                        <TimeRangeSlider/>
                     </Grid.Column>
                 </Grid>
             </div>
