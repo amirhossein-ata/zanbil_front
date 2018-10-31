c722e79e43a30bd9b8f2d933893ca691f7c137b4
Amirhossein <amirhossein1398021@gmail.com>
2018-10-31 20:52:02 +0330

    time range slider done

---
+++ src/views/componets/time_slider/time_slider.js
@@ -0,0 +1,76 @@
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tooltip from 'rc-tooltip';
+import Slider from 'rc-slider';
+import { Checkbox } from 'semantic-ui-react';
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
+class Time_range_slider extends React.Component{
+    state={
+        day_state:{
+            checked:true,
+            range :undefined
+        }
+    }
+    onRangeChange = (range) => {
+        console.log('heeeeeeeeeeeey ' ,range)
+        let newDayState = this.state.day_state
+        newDayState.range = range
+        this.setState(()=>({day_state:newDayState}))
+        const passedState = this.state
+        console.log('passed state is : ',passedState)
+        this.props.handleChange(this.props.id,passedState)
+    }
+    onCheckBoxChange = () => {
+        let newDayState = this.state.day_state
+        
+        newDayState.checked=!newDayState.checked
+        console.log(newDayState)
+        this.setState(()=>({day_state:newDayState}))
+        console.log(this.state)
+    }
+    render(){
+        return(
+            <div  style={{
+                display:'flex',
+                justifyContent:'space-around'
+                
+            }}>
+                <Checkbox
+                    checked={this.state.day_state.checked}
+                    fitted
+                    toggle 
+                    onChange={ this.onCheckBoxChange}
+                    />
+                    {this.state.day_state.checked ? (
+                        <div style={
+                            {
+                                width:'60%'
+                            }
+                        }>
+                            <Range
+                                min={6} 
+                                max={24} 
+                                defaultValue={[8, 18]} 
+                                onChange={value => this.onRangeChange(value)} 
+                                tipFormatter={value => `${value}`} 
+                            />
+                        </div>          
+                        
+                    ) : (
+                        <div style={
+                            {
+                                width:'60%'
+                            }}></div>
+                    )}     
+                
+            </div>
+        )
+    }
+}
+
+export default Time_range_slider
