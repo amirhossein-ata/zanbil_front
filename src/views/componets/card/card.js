bebe5bf484a57cc7721983f171c0997289815eb6
Amirhossein <amirhossein1398021@gmail.com>
2018-11-21 19:42:14 +0330

    some additions to landing page

--- src/views/componets/card/card.js
+++ src/views/componets/card/card.js
@@ -4,28 +4,65 @@ import { Card, Icon, Image,Button } from 'semantic-ui-react'
 const style = {
     height : '50vh'
 }
-const Card_component = (props) => (
-    
-    <Card color="teal" raised style={style}>
-        <Image size="big"  src="https://tehdooni.com/wp-content/uploads/2017/12/7715_%DA%A9%D8%A7%D9%81%D9%87-%D8%AA%D9%88-%DA%A9%D8%A7%D9%81%D9%87-%D8%AC%D9%87%D8%A7%D9%86-%D8%A2%D8%B1%D8%A7.jpg" />
-        <Card.Content>
-        <Card.Header>{props.header}</Card.Header>
-        <Card.Meta>
-            <span className='date'>{props.meta1}</span>
-        </Card.Meta>
-        <Card.Description>{props.description}</Card.Description>
-        </Card.Content>
-        {props.button && (
-            <Card.Content extra>
 
-                <div className='ui buttons'>
-                <Button primary>
-                    {props.button}
-                </Button>
-                </div>
-            </Card.Content>
+const amirhossein_style = (props) => ({
+    display:'flex', 
+    justifyContent:'space-between',
+    width:props.width? `${props.width}`: '30vw',
+    paddingRight:'3%',
+    paddingTop:'1%',
+    paddingBottom:'1%',
+    paddingLeft:'3%',
+    marginTop:props.marginTop ? `${props.marginTop}` : '',
+    marginBottom:props.marginBottom ? `${props.marginBottom}` : '',
+    marginLeft:props.marginLeft ? `${props.marginLeft}` : '',
+    marginRight:props.marginRight? `${props.marginRight}`:'',
+    border:`${props.border_color}` ?`1px solid ${props.border_color}` : '',
+    background:props.background ? props.background : '',
+    boxShadow:`2px 2px ${props.border_color}`
+})
+
+const Card_component = (props) => (
+    <div>
+    
+        {props.semantic && (
+            <Card color="teal" raised style={style}>
+                <Image size="big"  src="https://tehdooni.com/wp-content/uploads/2017/12/7715_%DA%A9%D8%A7%D9%81%D9%87-%D8%AA%D9%88-%DA%A9%D8%A7%D9%81%D9%87-%D8%AC%D9%87%D8%A7%D9%86-%D8%A2%D8%B1%D8%A7.jpg" />
+                <Card.Content>
+                <Card.Header>{props.header}</Card.Header>
+                <Card.Meta>
+                    <span className='date'>{props.meta1}</span>
+                </Card.Meta>
+                <Card.Description>{props.description}</Card.Description>
+                </Card.Content>
+                {props.button && (
+                    <Card.Content extra>
+        
+                        <div className='ui buttons'>
+                        <Button primary>
+                            {props.button}
+                        </Button>
+                        </div>
+                    </Card.Content>
+                )}
+            </Card>
+        
         )}
-    </Card>
+            
+        {props.amirhossein && (
+            <div className={props.className} style={amirhossein_style(props)}>
+            <div style={{height:'100%'}}>
+                <p style={{marginTop:'30%'}}>
+                    {props.card_text}
+                </p>
+            </div>
+            <img src={props.image} height={props.image_height} width={props.image_width}/>
+
+        </div>
+
+        )}
+        
+    </div>
 )
 
 export default Card_component
--- src/views/componets/landing_page/landing_page.js
+++ src/views/componets/landing_page/landing_page.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import {Header} from 'semantic-ui-react'
-import IconComponent from '../Icon/icon'
+import CardComponent from '../../componets/card/card'
 import timetable_icon from '../../../assessts/icons/calendar.svg'
+import booked_icon from '../../../assessts/icons/booked.svg'
+import introduction_icon from '../../../assessts/icons/online-shop.svg'
+import list_icon from '../../../assessts/icons/list.svg'
+import email_icon from '../../../assessts/icons/email.svg'
+import gold_icon from '../../../assessts/icons/gold-medal.svg'
+import silver_icon from '../../../assessts/icons/silver-medal.svg'
+import bronze_icon from '../../../assessts/icons/bronze-medal.svg'
+import tick_icon from '../../../assessts/icons/success.svg'
+import multiply_icon from '../../../assessts/icons/multiply.svg'
 class Landing_page extends React.Component{
     render(){
         return(
@@ -19,43 +28,214 @@ class Landing_page extends React.Component{
                     
                 </div>
                 <div style={{height:'auto',background:'white',padding:'3%'}}>
-                    <p>اگر صاحب کسب و کار هستید ما میتونیم به شما کمک کنیم :</p>
+                    <h4>اگر صاحب کسب و کار هستید ما میتونیم به شما کمک کنیم :</h4>
+                
                     <div style={{margin:'5%'}}>
-                        <div style={{display:'flex' , justifyContent:'space-between',width:'30vw',background:'rgba(233, 231, 227,.5)',padding:'3%'}}>
-                            <div style={{height:'100%'}}>
-                                <p style={{marginTop:'30%'}}>
-                                    کسب و کار خود را معرفی کنید     
-
-                                </p>
-                            </div>
-                            <img src={timetable_icon} height="150px" width="150px"/>
-
-                        </div>
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="کسب و کار خود را معرفی کنید"
+                            image={introduction_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="rgba(233, 231, 227,.5)"
+                        />
                         <br></br>
-                        <div style={{marginRight:'32%',display:'flex' , justifyContent:'space-between',width:'30vw',background:'rgba(233, 231, 227,.5)',padding:'3%'}}>
-                            <div style={{height:'100%'}}>
-                                <p style={{marginTop:'20%'}}>
-                                    سرویس های مختلف ایجاد کنید و برای هر کدام یک جدول زمانی ایجاد کنید                        
-                                </p>
-                            </div>
-                            <img src={timetable_icon} height="150px" width="150px"/>
-
-                        </div>
-                        <br></br>
-                        <div style={{marginRight:'65%',display:'flex' , justifyContent:'space-between',width:'30vw',background:'rgba(233, 231, 227,.5)',padding:'3%'}}>
-                            <div style={{height:'100%'}}>
-                                <p style={{marginTop:'30%'}}>
-                                    از سرویس رزرواسیون آنلاین استفاده کنید                        
-                                </p>
-                            </div>
-                            <img src={timetable_icon} height="150px" width="150px"/>
-
-                        </div>
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="سرویس های مختلف ایجاد کنید و برای هر کدام یک جدول زمانی ایجاد کنید"
+                            image={timetable_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="rgba(233, 231, 227,.5)"
+                            marginRight="31%"
+                            width="32vw"
+                        />
                         
+                        <br></br>
+
+
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="از سرویس رزرواسیون آنلاین استفاده کنید"
+                            image={booked_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="rgba(233, 231, 227,.5)"
+                            marginRight="65%"
+                        />
                     </div>
                 </div>
-                <div style={{height:'100vh',background:'#E7E8E7'}}></div>
-                <div style={{height:'100vh',background:'#4EAF55'}}></div>
+                <div style={{height:'auto',padding:'3%',background:'#E7E8E7'}}>
+                    <h4>اگر مشتری هستید ما میتونیم به شما کمک کنیم :</h4>
+                    
+                    <div style={{margin:'5%'}}>
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="در میان کسب و کارها جست و جو کنید"
+                            image={list_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="white"
+                        />
+                        <br></br>
+                    
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="از سرویس رزرواسیون آنلاین استفاده کنید"
+                            image={booked_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="white"
+                            marginRight="32%"
+                        />
+                    
+                        
+                        <br></br>
+                        <CardComponent 
+                            amirhossein={true}
+                            className="second_part_card"
+                            card_text="ایمیل های یادآوری دریافت کنید تا دیگر هیچ قراری را فراموش نکنید"
+                            image={email_icon}
+                            image_height="150px"
+                            image_width="150px"
+                            border_color="#BCDECF"
+                            background="white"
+                            marginRight="65%"
+                            width="32vw"
+                        />
+                
+
+                    </div>
+
+                </div>
+                <div style={{paddingTop:'2%' ,height:'100vh',background:'#BCDECF'}}>
+                    <div style={{textAlign:'center',}}>
+                        <h2>طرح های ما :</h2>                
+                    </div>
+                    <div style={{display:'flex',justifyContent:'center',paddingTop:'5%'}}>
+                        <div style={{textAlign:'right',width:'25vw', marginLeft:'2%' , height:'60vh',background:'white'}}>
+                            <div  style={{marginLeft:'auto',marginRight:'40%'}} >
+                                <img src={bronze_icon}width="80px" height="80px" />
+                                <h3>طرح برنزی</h3>
+                                <p>رایگان</p>
+                            </div>    
+                            <ul>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>تعداد سرویس : ۳</p>
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p> ایمیل یادآوری</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>پیامک یادآوری</p> <span style={{marginRight:'10px'}}><img  src={multiply_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>شخصی سازی صفحه ی کسب و کار</p> <span style={{marginRight:'10px'}}><img  src={multiply_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div style={{textAlign:'right',width:'25vw' ,marginLeft:'2%', height:'60vh',background:'white'}}>
+                            <div style={{marginLeft:'auto',marginRight:'40%'}}>         
+                                <img src={silver_icon}  width="80px" height="80px" />
+                                <h3>طرح نقره ای</h3>
+                                <p>۲۰,۰۰۰ تومان در ماه</p>
+                            </div>
+                            <ul>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>تعداد سرویس : ۱۰</p>
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p> ایمیل یادآوری</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>پیامک یادآوری</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>شخصی سازی صفحه ی کسب و کار</p> <span style={{marginRight:'10px'}}><img  src={multiply_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div style={{textAlign:'right',width:'25vw' , height:'60vh',background:'white'}}>
+                            
+
+                            <div style={{marginLeft:'auto',marginRight:'40%'}} >
+                                <img src={gold_icon} width="80px" height="80px" />                        
+                                <h3>طرح طلایی</h3>
+                                <p>۵۰,۰۰۰ تومان در ماه</p>
+                            </div>
+
+                            <ul>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>تعداد سرویس :نامحدود</p>
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p> ایمیل یادآوری</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>پیامک یادآوری</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+
+                                <br></br>
+                                <li>
+                                    <div style={{display:'flex'}}>
+                                        <p>شخصی سازی صفحه ی کسب و کار</p> <span style={{marginRight:'10px'}}><img  src={tick_icon} height="30px"  width="30px"/></span>
+                                    
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>      
+                    </div>
+                </div>
 
             </div>
         )
