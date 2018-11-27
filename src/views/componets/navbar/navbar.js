60e67586f0a9873019a1649eb2188ac5ba637841
Amirhossein <amirhossein1398021@gmail.com>
2018-11-27 21:24:09 +0330

    navbar refactored

--- src/views/componets/navbar/navbar.js
+++ src/views/componets/navbar/navbar.js
@@ -1,267 +1,151 @@
-// import React, { Component } from 'react'
-// import {connect} from 'react-redux'
-// import { Dropdown , Menu ,Container,Icon,Image,Sidebar,Responsive} from 'semantic-ui-react'
-// import LoginForm from '../login&signup/loginForm'
-// import SignupForm from '../login&signup/signup_form'
-// import ModalComponent from '../modal/Modal'
-// import {categories} from '../../../core/constants'
-// import * as session_actions  from '../../../core/login&signup/session_actions'
-// import * as category_page_actions from '../../../core/category_page/category_page_actions'
-// import {change_panel} from '../../../core/main_page/active_panel_actions'
-// import {open_search_form} from '../../../core/search/search_actions'
-
-// const LoginModal = ModalComponent('ورود')(LoginForm)
-// const SignUpModal = ModalComponent('ثبت نام')(SignupForm)
+import React from 'react'
+import { Dropdown , Menu ,Container,Icon,Image,Sidebar,Responsive,Grid, GridColumn, Segment } from 'semantic-ui-react';
+import LoginForm from '../login&signup/loginForm'
+import SignupForm from '../login&signup/signup_form'
+import ModalComponent from '../modal/Modal'
+import {categories} from '../../../core/constants'
 
 
-// const NavBarMobile = ({
-//     onPusherClick,
-//     onToggle,
-//     visible,
-//     acount_page_click,
-//     logout_click,
-//     handle_home_click,
-//     handle_category_click,
-//     showSearch,
-//     logged_in,
-//     Component
-// }) => (
-//     <Sidebar.Pushable>
-//         <Sidebar
-//             as={Menu}
-//             animation="overlay"
-//             icon="labeled"
-//             vertical
-//             visible={visible}
-//         >
-//                 <Menu.Item
-//                     onClick={handle_home_click}
-//                 >
-//                     خانه
-//                 </Menu.Item>
+const LoginModal = ModalComponent('ورود')(LoginForm)
+const SignUpModal = ModalComponent('ثبت نام')(SignupForm)
 
-//                 <Dropdown text="دسته بندی ها" pointing className='link item' >
-//                     <Dropdown.Menu >
-//                         {categories.map((category) => (
-//                             <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
-//                         ))}
 
-//                     </Dropdown.Menu>
-//                 </Dropdown>    
-            
-//                 <Menu.Item
-//                     onClick={()=>showSearch()}        
-//                 >
-//                     جست و جو
-//             </Menu.Item>
+export const NavBarMobile = ({
+    onPusherClick,
+    onToggle,
+    visible,
+    acount_page_click,
+    logout_click,
+    handle_home_click,
+    handle_category_click,
+    showSearch,
+    logged_in,
+    Component
+}) => (
+    <Sidebar.Pushable>
+        <Sidebar
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            vertical
+            visible={visible}
+            direction="left"
+        >   
+                <Menu.Item
+                    onClick={handle_home_click}
+                >
+                    خانه
+                </Menu.Item>
+              
+    
 
-//                 {!logged_in && (
-//                     <Menu.Item>   
-//                     <span style={{
-//                         display:'flex'
-//                     }}>
-//                         <LoginModal />
-//                         <SignUpModal />
-                        
-//                     </span>
-//                 </Menu.Item>
-//                 )}
-//         </Sidebar>
-//         <Sidebar.Pusher
-//             dimmed={visible}
-//             onClick={onPusherClick}
-//             style={{ minHeight: "100vh" }}
-//         >
-//             <Menu fixed="top">
-//                 <Menu.Item>
-//                     <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
-//                 </Menu.Item>
-//                 {logged_in && (
-//                         <Dropdown  icon="user circle outilne" button pointing className='link item' >
-//                             <Dropdown.Menu >
-//                                 <Dropdown.Item onClick={acount_page_click}>حساب کاربری</Dropdown.Item>
-//                                 <Dropdown.Item onClick={logout_click} >خروج</Dropdown.Item>
-//                             </Dropdown.Menu>
-//                         </Dropdown>        
+                <Menu.Item
+                    onClick={()=>showSearch()}        
+                >
+                    جست و جو
+                </Menu.Item>
+
+                    {!logged_in && (
+                        <Menu.Item>   
+                        <span style={{
+                            display:'flex'
+                        }}>
+                            <LoginModal />
+                            <SignUpModal />
+                            
+                        </span>
+                    </Menu.Item>
+                    )}
+
+        </Sidebar>
+        <Sidebar.Pusher
+            dimmed={visible}
+            onClick={onPusherClick}
+            style={{ minHeight: "100vh" }}
+        >
+            <Menu fixed="top">
+                <Menu.Item>
+                    <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+                </Menu.Item>
+                {logged_in && (
+                        <Dropdown  icon="user circle outilne" button pointing className='link item' >
+                            <Dropdown.Menu >
+                                <Dropdown.Item onClick={acount_page_click}>حساب کاربری</Dropdown.Item>
+                                <Dropdown.Item onClick={logout_click} >خروج</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>        
                 
-//                 )}
-//                 <Menu.Item  position="left" onClick={onToggle}>
-//                     <Icon name="sidebar" />
-//                 </Menu.Item>
-                
-//             </Menu>
-//             <Component />
+                )}
+                <Dropdown text="دسته بندی ها" item pointing className='link item' >
+                    <Dropdown.Menu >
+                        {categories.map((category) => (
+                            <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
+                        ))}
 
-//         </Sidebar.Pusher>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item  position="left" onClick={onToggle}>
+                    <Icon name="sidebar" />
+                </Menu.Item>
+                
+            </Menu>
+            <br></br><br></br><br></br><br></br>
+            <Component />
+
+        </Sidebar.Pusher>
   
-//     </Sidebar.Pushable>
-// )
+    </Sidebar.Pushable>
+)
 
-// const NavBarDesktop = ({
-//     acount_page_click,
-//     logout_click,
-//     handle_home_click,
-//     handle_category_click,
-//     showSearch,
-//     logged_in
-// }) => (
-//     <Menu fixed="top" size="small">
+export const NavBarDesktop = ({
+    acount_page_click,
+    logout_click,
+    handle_home_click,
+    handle_category_click,
+    showSearch,
+    logged_in
+}) => (
+    <Menu fixed="top" size="small">
         
-//         <Menu.Item>
-//             <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
-//         </Menu.Item>
-//         <Menu.Item
-//             onClick={handle_home_click}
-//         >
-//             خانه
-//         </Menu.Item>
+        <Menu.Item>
+            <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+        </Menu.Item>
+        <Menu.Item
+            onClick={handle_home_click}
+        >
+            خانه
+        </Menu.Item>
 
-//         <Dropdown text="دسته بندی ها" item pointing className='link item' >
-//             <Dropdown.Menu >
-//                 {categories.map((category) => (
-//                     <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
-//                 ))}
+        <Dropdown text="دسته بندی ها"  item pointing className='link item' >
+            <Dropdown.Menu >
+                {categories.map((category) => (
+                    <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
+                ))}
 
-//             </Dropdown.Menu>
-//         </Dropdown>    
+            </Dropdown.Menu>
+        </Dropdown>    
 
-//         <Menu.Item
-//             onClick={()=>showSearch()}        
-//         >
-//             جست و جو
-//         </Menu.Item>
-//         <div style={{marginRight:'auto'}}>
-//             <Menu.Menu position="left">
-//                 {logged_in ? (
-//                     <Menu.Item>
-//                         <Icon onClick={acount_page_click} name="user"  size="big"/>
-//                         <Icon onClick={logout_click} name="log out" flipped="horizontally" size="big"/>
-//                     </Menu.Item>
-//                 ):(
-//                     <Menu.Item>   
-//                         <LoginModal />
-//                         <SignUpModal />
-//                     </Menu.Item>
-//                 )}
+        <Menu.Item
+            onClick={()=>showSearch()}        
+        >
+            جست و جو
+        </Menu.Item>
+        <div style={{marginRight:'auto'}}>
+            <Menu.Menu position="left">
+                {logged_in ? (
+                    <Menu.Item>
+                        <Icon onClick={acount_page_click} name="user"  size="big"/>
+                        <Icon onClick={logout_click} name="log out" flipped="horizontally" size="big"/>
+                    </Menu.Item>
+                ):(
+                    <Menu.Item>   
+                        <LoginModal />
+                        <SignUpModal />
+                    </Menu.Item>
+                )}
                     
                     
-//             </Menu.Menu>
-//         </div>
-//     </Menu>
-// );
-  
-    
-// class NavBar extends Component {
-//     state = {
-//         visible: false,
-//         activeItem: 'home',
-//         show_search : false
-//     };
-//     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-//     logout_click = () => {
-//         this.props.logout()
-//     }
-//     acount_page_click= () =>{
-//         this.props.change_panel("account_page")
-//     }
-//     handle_category_click=(category_id)=>{
-//         this.props.change_panel('category')
-//         this.props.get_category_businesses(category_id)
-//     }
-
-//     handle_home_click = () => {
-//         this.props.change_panel('landing_page')
-//     }
-//     showSearch = () => {
-//         this.props.change_panel('search')
-//         this.props.open_search_form()
-//     }
-
-//     handlePusher = () => {
-//         const { visible } = this.state;
-    
-//         if (visible) this.setState({ visible: false });
-//     };
-    
-//     handleToggle = () => this.setState({ visible: !this.state.visible });
-//     get_active_panel=() => {
-//         switch (this.props.active_panel) {
-            
-//             case 'category':
-//                 return <CategoryPage />;
-//             case 'business_page':
-//                 return <BusinessPage />;
-//             case 'add_business_page':
-//                 return <AddBusinessPage />
-//             case 'add_service_page':
-//                 return <AddServicePage />
-//             case 'service_page' :
-//                 return <ServicePage />
-//             case 'search' :
-//                 return <SearchPage/>
-//             case"account_page":
-//                 return <AccountPage/>
-//             case 'landing_page':
-//                 return <Landing_page/>
-//             default:
-//                 return ''
-//         }
-//     }
-
-//     render(){
-        
-//         const { visible } = this.state;
-    
-//         return (
-//             <div>
-//             <Responsive {...Responsive.onlyMobile}>
-//                 <NavBarMobile
-//                     onPusherClick={this.handlePusher}
-//                     onToggle={this.handleToggle}
-//                     visible={visible}
-//                     acount_page_click={this.acount_page_click}
-//                     logout_click={this.logout_click}
-//                     handle_home_click={this.handle_home_click}
-//                     handle_category_click={this.handle_category_click}
-//                     showSearch={this.showSearch}
-//                     logged_in={this.props.logged_in}
-//                     Component={this.get_active_panel}
-//                 >
-//                 </NavBarMobile>
-//             </Responsive>
-//             <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-                // <NavBarDesktop 
-                //     acount_page_click={this.acount_page_click}
-                //     logout_click={this.logout_click}
-                //     handle_home_click={this.handle_home_click}
-                //     handle_category_click={this.handle_category_click}
-                //     showSearch={this.showSearch}   
-                //     logged_in={this.props.logged_in} 
-                // />
-//             </Responsive>
-//             </div>
-//         );
-//     }
-    
-// }
-  
-
-
-// const mapStateToProps = (state) => {
-//     console.log(state)
-//     return{
-//         logged_in : state.session_reducer.logged_in,
-//         active_panel:state.active_panel_reducer.active_panel
-//     }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//     return{
-//         logout : () => dispatch(session_actions.logout()),
-//         get_category_businesses: (category_id) => dispatch(category_page_actions.get_category_businesses(category_id)),
-//         change_panel:(panel_name) => dispatch(change_panel(panel_name)),
-//         open_search_form :() => dispatch(open_search_form())
-//     }
-// }
-
-// export default connect(mapStateToProps,mapDispatchToProps)(NavBar)
+            </Menu.Menu>
+        </div>
+    </Menu>
+);
--- src/views/pages/main_page.js
+++ src/views/pages/main_page.js
@@ -18,149 +18,7 @@ import * as session_actions  from '../../core/login&signup/session_actions'
 import * as category_page_actions from '../../core/category_page/category_page_actions'
 import {change_panel} from '../../core/main_page/active_panel_actions'
 import {open_search_form} from '../../core/search/search_actions'
-const LoginModal = ModalComponent('ورود')(LoginForm)
-const SignUpModal = ModalComponent('ثبت نام')(SignupForm)
-
-
-const NavBarMobile = ({
-    onPusherClick,
-    onToggle,
-    visible,
-    acount_page_click,
-    logout_click,
-    handle_home_click,
-    handle_category_click,
-    showSearch,
-    logged_in,
-    Component
-}) => (
-    <Sidebar.Pushable>
-        <Sidebar
-            as={Menu}
-            animation="overlay"
-            icon="labeled"
-            vertical
-            visible={visible}
-            direction="left"
-        >   
-                <Menu.Item
-                    onClick={handle_home_click}
-                >
-                    خانه
-                </Menu.Item>
-              
-    
-
-                <Menu.Item
-                    onClick={()=>showSearch()}        
-                >
-                    جست و جو
-                </Menu.Item>
-
-                    {!logged_in && (
-                        <Menu.Item>   
-                        <span style={{
-                            display:'flex'
-                        }}>
-                            <LoginModal />
-                            <SignUpModal />
-                            
-                        </span>
-                    </Menu.Item>
-                    )}
-
-        </Sidebar>
-        <Sidebar.Pusher
-            dimmed={visible}
-            onClick={onPusherClick}
-            style={{ minHeight: "100vh" }}
-        >
-            <Menu fixed="top">
-                <Menu.Item>
-                    <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
-                </Menu.Item>
-                {logged_in && (
-                        <Dropdown  icon="user circle outilne" button pointing className='link item' >
-                            <Dropdown.Menu >
-                                <Dropdown.Item onClick={acount_page_click}>حساب کاربری</Dropdown.Item>
-                                <Dropdown.Item onClick={logout_click} >خروج</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>        
-                
-                )}
-                <Dropdown text="دسته بندی ها" item pointing className='link item' >
-                    <Dropdown.Menu >
-                        {categories.map((category) => (
-                            <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
-                        ))}
-
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Menu.Item  position="left" onClick={onToggle}>
-                    <Icon name="sidebar" />
-                </Menu.Item>
-                
-            </Menu>
-            <Component />
-
-        </Sidebar.Pusher>
-  
-    </Sidebar.Pushable>
-)
-
-const NavBarDesktop = ({
-    acount_page_click,
-    logout_click,
-    handle_home_click,
-    handle_category_click,
-    showSearch,
-    logged_in
-}) => (
-    <Menu fixed="top" size="small">
-        
-        <Menu.Item>
-            <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
-        </Menu.Item>
-        <Menu.Item
-            onClick={handle_home_click}
-        >
-            خانه
-        </Menu.Item>
-
-        <Dropdown text="دسته بندی ها"  item pointing className='link item' >
-            <Dropdown.Menu >
-                {categories.map((category) => (
-                    <Dropdown.Item onClick={()=>handle_category_click(category.value)} >{category.text}</Dropdown.Item>
-                ))}
-
-            </Dropdown.Menu>
-        </Dropdown>    
-
-        <Menu.Item
-            onClick={()=>showSearch()}        
-        >
-            جست و جو
-        </Menu.Item>
-        <div style={{marginRight:'auto'}}>
-            <Menu.Menu position="left">
-                {logged_in ? (
-                    <Menu.Item>
-                        <Icon onClick={acount_page_click} name="user"  size="big"/>
-                        <Icon onClick={logout_click} name="log out" flipped="horizontally" size="big"/>
-                    </Menu.Item>
-                ):(
-                    <Menu.Item>   
-                        <LoginModal />
-                        <SignUpModal />
-                    </Menu.Item>
-                )}
-                    
-                    
-            </Menu.Menu>
-        </div>
-    </Menu>
-);
-
+import {NavBarDesktop , NavBarMobile} from '../componets/navbar/navbar'
 
 class Main_page extends React.Component{
     state = {
@@ -260,6 +118,8 @@ class Main_page extends React.Component{
                             showSearch={this.showSearch}   
                             logged_in={this.props.logged_in} 
                         />
+                        <br></br><br></br><br></br>
+
                         {this.props.active_panel==='landing_page' ? (
                             <Landing_page />
                         ) : (
