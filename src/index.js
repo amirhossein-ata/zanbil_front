8e2b1fa7852c4b8fb726eb78a06dbb52b4f2413a
Amirhossein <amirhossein1398021@gmail.com>
2018-12-11 23:12:41 +0330

    added animation to cards

--- src/index.js
+++ src/index.js
@@ -13,6 +13,55 @@ const jsx = (
 )
 ReactDOM.render(jsx, document.getElementById('root'));
 
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import posed from 'react-pose';
+// // import './styles.css';
+
+
+// const Card = () =>(
+//     <div>
+//         <h4>
+//             card header
+//         </h4>
+//         <p>
+//             card text
+//         </p>
+//     </div>
+// )
+// const Box = posed.div({
+//     hidden: { opacity: 0 },
+//     visible: { opacity: 1 }
+//   });
+  
+// const boxStyle = {
+//     width: '100px',
+//     height: '100px',
+//     background: '#ff1c68',
+//     transformOrigin: '50% 50%'
+//   }
+  
+// class Example extends React.Component {
+//   state = { isVisible: true };
+
+
+//   render() {
+//     const { isVisible } = this.state;
+//     return(
+//          <Box 
+//             style={boxStyle}  
+//             pose={isVisible ? 'visible' : 'hidden'} 
+//         >
+//             <Card/> 
+//         </Box>
+//     )
+//   }
+// }
+
+// ReactDOM.render(<Example />, document.getElementById('root'));
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
--- src/views/componets/card/card.js
+++ src/views/componets/card/card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Icon, Image,Button,Rating, Divider } from 'semantic-ui-react'
+import posed from 'react-pose'
 
 const style = {
     height : '50vh'
@@ -28,90 +29,125 @@ const info_style = (props) => ({
     height:'50vh',
 
 })
-const Card_component = (props) => (
-    <div>
-    
-        {props.semantic && (
-            <Card color="teal" raised style={style}>
-                <Card.Content>
-            
-                <Card.Header>{props.header}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>{props.meta1}</span>
-                </Card.Meta>
-                <Card.Description>{props.description}</Card.Description>
-                </Card.Content>
-                {props.button && (
-                    <Card.Content extra>
-        
-                        <div className='ui buttons'>
-                        <Button primary>
-                            {props.button}
-                        </Button>
-                        </div>
-                    </Card.Content>
-                )}
-            </Card>
-        
-        )}
-        {props.info && (
-            <Card raised fluid  color="teal" style={info_style(props)}>
-                <Image size="massive" src={props.image} />
-                <Card.Content textAlign="right">
-                    <Card.Header>{props.header}</Card.Header>
-                    <Card.Meta>
-                        {props.rating && (
-                            <Rating disabled icon="star" defaultRating={props.rating%5} maxRating={5}/>            
 
-                        )}
-                        {props.date && (
-                            <p>{props.date}</p>
-                        )}
-                    </Card.Meta>
-                    <Card.Description>{props.description}</Card.Description>
-                </Card.Content>
-            </Card>
-        )}
-        
-        {props.report && (
-            <Card raised fluid>
-                <Card.Content style={{background:`${props.color}`}}>
-                    <Card.Header >
-                        <span style={{display:'flex',justifyContent:'space-between',color:'white'}}>
-                            <p>{props.header}</p>
-                            <Icon name="exclamation" />
-                        </span>
-                    </Card.Header>
-                </Card.Content>
-                <Card.Content>
-                    <Card.Description>
-                        <span style={{display:'flex'}}>
-                            <h3>{props.value}</h3>
-                            {props.percentage > 0 ? (
-                                    <Icon name="caret up" color="green">{props.percentage}%</Icon>
-                                ) : (
-                                    <Icon name="caret down" color="red">{props.percentage}%</Icon>
+const AnimatedCard = posed.div({
+    // hoverable: true,
+    pressable: true,
+    init: { scale: 1 },
+    press: { scale: 0.8 }
+    // init: {
+    //     scale: 1,
+    //     boxShadow: '0px 0px 0px rgba(0,0,0,0)'
+    // },
+    // press: { scale: 0.8 }
+
+    // hover: {
+    //     scale: 1.05,
+    //     boxShadow: '0px 5px 10px rgba(0,0,0,0.2)'
+    // } 
+});
+
+
+class Card_component extends React.Component{
+    state={
+        mouseOver:false
+    }
+    render(){
+        return(
+            <div 
+                onMouseEnter={()=>this.setState(()=>({mouseOver:true}))}
+                onMouseLeave={()=>this.setState(()=>({mouseOver:false}))}    
+            >
+                <AnimatedCard
+                    pose={this.state.mouseOver ? 'visible' : 'hidden'} 
+
+                >
+                
+                    {this.props.semantic && (
+                        <Card color="teal" raised style={style} link>
+                            <Card.Content>
+                        
+                            <Card.Header>{this.props.header}</Card.Header>
+                            <Card.Meta>
+                                <span className='date'>{this.props.meta1}</span>
+                            </Card.Meta>
+                            <Card.Description>{this.props.description}</Card.Description>
+                            </Card.Content>
+                            {this.props.button && (
+                                <Card.Content extra>
+                    
+                                    <div className='ui buttons'>
+                                    <Button primary>
+                                        {this.props.button}
+                                    </Button>
+                                    </div>
+                                </Card.Content>
                             )}
-                        </span>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                        {props.extra}
-                </Card.Content>
+                        </Card>
+                    
+                    )}
+                    {this.props.info && (
+                            <Card raised fluid  color="teal" style={info_style(this.props)} link>
+                                <Image size="massive" src={this.props.image} />
+                                <Card.Content textAlign="right">
+                                    <Card.Header>{this.props.header}</Card.Header>
+                                    <Card.Meta>
+                                        {this.props.rating && (
+                                            <Rating disabled icon="star" defaultRating={this.props.rating%5} maxRating={5}/>            
             
-            </Card>
-        )}
-
-        {props.amirhossein && (
-            <div className={props.className} style={amirhossein_style(props)}>
-                <p>{props.card_text}</p>
-                <img src={props.image} height={props.image_height} width={props.image_width}/>
-
+                                        )}
+                                        {this.props.date && (
+                                            <p>{this.props.date}</p>
+                                        )}
+                                    </Card.Meta>
+                                    <Card.Description>{this.props.description}</Card.Description>
+                                </Card.Content>
+                            </Card>
+                       
+                    )}
+                    
+                    {this.props.report && (
+                        <Card raised fluid >
+                            <Card.Content style={{background:`${this.props.color}`}}>
+                                <Card.Header >
+                                    <span style={{display:'flex',justifyContent:'space-between',color:'white'}}>
+                                        <p>{this.props.header}</p>
+                                        <Icon name="exclamation" />
+                                    </span>
+                                </Card.Header>
+                            </Card.Content>
+                            <Card.Content>
+                                <Card.Description>
+                                    <span style={{display:'flex'}}>
+                                        <h3>{this.props.value}</h3>
+                                        {this.props.percentage > 0 ? (
+                                                <Icon name="caret up" color="green">{this.props.percentage}%</Icon>
+                                            ) : (
+                                                <Icon name="caret down" color="red">{this.props.percentage}%</Icon>
+                                        )}
+                                    </span>
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                    {this.props.extra}
+                            </Card.Content>
+                        
+                        </Card>
+                    )}
+            
+                    {this.props.amirhossein && (
+                        <div className={this.props.className} style={amirhossein_style(this.props)}>
+                            <p>{this.props.card_text}</p>
+                            <img src={this.props.image} height={this.props.image_height} width={this.props.image_width}/>
+            
+                        </div>
+            
+                    )}
+                </AnimatedCard>
             </div>
-
-        )}
-        
-    </div>
-)
+    
+        )
+    }
+}
 
 export default Card_component
