f9fd986fcec30686228c36950cfb1c1b54c59b8b
Amirhossein <amirhossein1398021@gmail.com>
2018-12-21 12:16:11 +0330

    bug on show picture fixed

--- src/views/pages/category_page.js
+++ src/views/pages/category_page.js
@@ -60,7 +60,7 @@ class Category_page extends React.Component{
                                     <div onClick={()=>this.on_business_click(business)}>
                                         <CardComponent
                                             info={true}
-                                            image={business.pictures ?  require(`../../assessts/ZanbilBackEnd/uploads/${business.pictures[business.pictures.length - 1].id}`) : "https://tehdooni.com/wp-content/uploads/2017/12/7715_%DA%A9%D8%A7%D9%81%D9%87-%D8%AA%D9%88-%DA%A9%D8%A7%D9%81%D9%87-%D8%AC%D9%87%D8%A7%D9%86-%D8%A2%D8%B1%D8%A7.jpg"}
+                                            image={business.pictures.length !== 0 ?  require(`../../assessts/ZanbilBackEnd/uploads/${business.pictures[business.pictures.length - 1].id}`) : "https://tehdooni.com/wp-content/uploads/2017/12/7715_%DA%A9%D8%A7%D9%81%D9%87-%D8%AA%D9%88-%DA%A9%D8%A7%D9%81%D9%87-%D8%AC%D9%87%D8%A7%D9%86-%D8%A2%D8%B1%D8%A7.jpg"}
                                             header={business.name}
                                             rating={business.rating}
                                             description={business.description}
