6ed8e20fbaf6d7ee4e2f145fb9665ad077ee6308
Amirhossein <amirhossein1398021@gmail.com>
2019-01-30 00:45:18 +0330

    preview of timetable on addService form implemented

--- src/views/componets/add_service/preview_timetable_on_addService.js
+++ src/views/componets/add_service/preview_timetable_on_addService.js
@@ -2,18 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PreviewTimetable from '../timetable/preview_timetable';
 import moment from 'jalali-moment'
+import * as add_service_actions from '../../../core/add_service/add_service_actions';
+import { Grid, Button, Header, Divider } from 'semantic-ui-react';
+import {change_panel} from '../../../core/main_page/active_panel_actions'
+
 class Preview_timetable_on_addService extends React.Component{
     state = {
-        sanses:[
-            [],[],[],[],[],[],[]
-        ],
+        sanses:[[],[],[],[],[],[],[]],
         first_range_error:false,
         second_range_error:false
   
     }
 
     componentDidMount(){
-        let sanses = this.state.sanses
         if(this.props.days){
             this.generateSanses(this.props.days)
         }
@@ -25,7 +26,7 @@ class Preview_timetable_on_addService extends React.Component{
         let sanses = [[],[],[],[],[],[],[]]
         days.map((day, index) => {
             let start_time = moment({hour: day.start_time})
-            let duration = day.duration.split(':')
+            let duration = day.duration ? day.duration.split(':') : ['1','0']
             let duration_hour = parseInt(duration[0],10)
             let duration_minute = parseInt(duration[1],10)
             duration = moment({hour:duration_hour,minute:duration_minute})
@@ -33,178 +34,92 @@ class Preview_timetable_on_addService extends React.Component{
             let temp = start_time
             let rest_start_time = moment({hour:day.rest_start_time})
             let rest_end_time = moment({hour:day.rest_end_time})
-            while(temp <= end_time){
+            let counter = 0
+            while(temp <= end_time && counter < 100){
                 let start_whitin = (temp > rest_start_time) && (temp < rest_end_time)
                 let start_time = temp.hour().toString() + ':'+temp.minutes().toString() 
                 temp.add(duration_hour,'hours')
                 temp.add(duration_minute,'minutes')
                 let end_time = temp.hour().toString() + ':'+temp.minutes().toString()
-                console.log(start_time,end_time)
                 let end_whitin = (temp > rest_start_time) && (temp < rest_end_time)
                 
                 if( !(start_whitin || end_whitin)){
-                    sanses[index].push({start_time:start_time,end_time:end_time,capacity:day.capacity})
+                    sanses[index].push({id:counter,is_reserved:false,sans:{weekday:index,start_time:start_time,end_time:end_time,capacity:day.capacity}})
+                    counter ++
                 }
+
             }  
         })
+
         console.log(sanses)
+        this.setState(()=>({sanses:[sanses]}))
+        console.log('state is : ', this.state)
     }
     onSubmit = () => {
-
+        console.log(this.state.sanses,this.props.informations,this.props.business_id)
+        this.props.add_service(this.props.informations,this.state.sanses[0],this.props.business_id)
+        this.props.change_panel('dashboard')
     }
 
-    validate_first_range = (sansinfo) => {
-        const time = sansinfo.start_time;
-        
-        let temp_sanses= this.state.sanses;
-        const day=temp_sanses[0][sansinfo.weekday];
-        let prev_sans=undefined;
-        console.log("sansinfo is:",sansinfo);
-        console.log("FUCK OFFFF!!!");
-        if(sansinfo.sans_num !== 0 ){
-            prev_sans = day[sansinfo.sans_num-1];
-        }
-        if(prev_sans){
-             console.log("I've got news for u baby")
-             if(((parseInt(prev_sans.sans.start_time.slice(0,3),10) > parseInt(time.slice(0,3) ,10)) || ((parseInt(prev_sans.sans.start_time.slice(0,3),10) === parseInt(time.slice(0,3) ,10)) && ((parseInt(prev_sans.sans.start_time.slice(3),10) > parseInt(time.slice(3) ,10))))) || !(/[0-9.]/.test(time))){
-                console.log("Guss what?!\n");     
-                this.setState(() => ({first_range_error:true}));
-
-             }
-             else{
-                this.setState(() => ({first_range_error:false}));
-             }
-        }
-        else{
-            if((/[0-9.]/.test(time))){
-                this.setState(() => ({first_range_error:true}));
-            }
-            else{
-                this.setState(() => ({first_range_error:false}));
-             }
-
-        }
-        
-
-    }
-    validate_second_range = (sansinfo) => {
-        const time = sansinfo.end_time;
-        
-        let temp_sanses= this.state.sanses;
-        const day=temp_sanses[0][sansinfo.weekday];
-        let next_sans=undefined;
-        if(sansinfo.sans_num !== 0 ){
-            next_sans = day[sansinfo.sans_num+1];
-        }
-        if(next_sans){
-             console.log("I've got news for u baby")
-             if(((parseInt(next_sans.sans.end_time.slice(0,3),10) < parseInt(time.slice(0,3) ,10)) || ((parseInt(next_sans.sans.end_time.slice(0,3),10) === parseInt(time.slice(0,3) ,10)) && ((parseInt(next_sans.sans.end_time.slice(3),10) < parseInt(time.slice(3) ,10))))) || !(/[0-9.]/.test(time))){
-                console.log("Guss who's back?!\n");     
-                this.setState(() => ({second_range_error:true}));
-
-             }
-             else{
-                this.setState(() => ({second_range_error:false}));
-             }
-        }
-        else{
-            if((/[0-9.]/.test(time))){
-                this.setState(() => ({second_range_error:true}));
-            }
-            else{
-                this.setState(() => ({second_range_error:false}));
-             }
-
-        }
-        
-
-    }
+    validate_first_range = (sansinfo) => {}
+    validate_second_range = (sansinfo) => {}
+    deleteSans = (sansinfo) => {
+        let temp_sanses = this.state.sanses; 
+        console.log(temp_sanses, sansinfo)
+        temp_sanses[0][sansinfo.weekday].splice(temp_sanses[0][sansinfo.weekday].findIndex(sans => sans.id === sansinfo.sans_num),1);
+        this.setState(()=>({sanses:temp_sanses}))
+        console.log(this.state)
     
+    }
     onConfirmChange= (sansinfo) => {
         let temp_sanses= this.state.sanses;
-        const day=temp_sanses[0][sansinfo.weekday];
-        let prev_sans=undefined;
-        let next_sans=undefined;
-        let temp_sans = day[sansinfo.sans_num];
-        let temp_modified = this.state.modified_sanses;
-        
-        if(sansinfo.sans_num !== 0 ){
-             prev_sans = day[sansinfo.sans_num-1];
-         }
-        if (sansinfo.sans_num !== day.length-1){
-             next_sans = day[sansinfo.sans_num+1];
-        }
-        if(prev_sans){
-            //console.log("prevsans:",prev_sans)
-             if((parseInt(prev_sans.sans.end_time.slice(0,3),10) > parseInt(sansinfo.start_time.slice(0,3) ,10)) || ((parseInt(prev_sans.sans.end_time.slice(0,3),10) === parseInt(sansinfo.start_time.slice(0,3) ,10)) && ((parseInt(prev_sans.sans.end_time.slice(3),10) > parseInt(sansinfo.start_time.slice(3) ,10))))){
-                     let prev_temp_sans=prev_sans;
-                     prev_temp_sans.sans.end_time = sansinfo.start_time;
-                     prev_temp_sans.sans.is_deleted = 0;
-                     prev_temp_sans.sans.sans_id = prev_temp_sans.sans.id;
-                     temp_sanses[0][sansinfo.weekday][sansinfo.sans_num-1] = prev_temp_sans;
-                      
-                     temp_modified.push(prev_temp_sans.sans);
-                     
-                     
-
-             }
-        }
-        if(next_sans){
-            console.log("next sans is:",next_sans.sans)
-         
-         if((parseInt(next_sans.sans.start_time.slice(0,3),10) < parseInt(sansinfo.end_time.slice(0,3) ,10)) || ((parseInt(next_sans.sans.start_time.slice(0,3),10) === parseInt(sansinfo.end_time.slice(0,3) ,10)) && ((parseInt(next_sans.sans.start_time.slice(3),10) < parseInt(sansinfo.end_time.slice(3) ,10))))){
-             let next_temp_sans=next_sans;
-             next_temp_sans.sans.start_time = sansinfo.end_time;
-             next_temp_sans.sans.sans_id = next_temp_sans.sans.id;
-             next_temp_sans.sans.is_deleted = 0;
-             temp_sanses[0][sansinfo.weekday][sansinfo.sans_num+1] = next_temp_sans; 
-             temp_modified.push(next_temp_sans.sans);
-     }
-
-        }
-        temp_sanses[0][sansinfo.weekday][sansinfo.sans_num] = temp_sans; 
-        temp_sans.sans.start_time = sansinfo.start_time;
-        temp_sans.sans.end_time = sansinfo.end_time;
-        temp_sans.sans.is_deleted = 0;
-        console.log("tempsans is:",temp_sans)
-        temp_sans.sans.sans_id = temp_sans.sans.id;
-        temp_modified.push(temp_sans.sans);
-        this.setState(() => ({
-            modified_sanses : temp_modified,
-            sanses:temp_sanses
-        }))
-         console.log("after setState:",this.state)
-     
-    }
-    deleteSans = (sansinfo) => {
-        let temp_sanses= this.state.sanses;
-        
-        const day=temp_sanses[0][sansinfo.weekday];
-        let temp_sans = day[sansinfo.sans_num];
-        temp_sans.is_deleted = 1;
-        let temp_modified = this.state.modified_sanses;
-        temp_modified.push(temp_sans.sans);
-        temp_sanses[0][sansinfo.weekday].splice(sansinfo.sans_num,1);
-        this.setState(() => ({
-            modified_sanses : temp_modified,
-            sanses:temp_sanses
-        }))
-
+        let index = temp_sanses[0][sansinfo.weekday].findIndex(sans => sans.id === sansinfo.sans_num)
+        console.log(index)
+        console.log(temp_sanses[0][sansinfo.weekday][index].sans.start_time , temp_sanses[0][sansinfo.weekday][index].sans.end_time)
+        temp_sanses[0][sansinfo.weekday][index].sans.start_time = sansinfo.start_time
+        temp_sanses[0][sansinfo.weekday][index].sans.end_time = sansinfo.end_time
+        console.log(temp_sanses[0][sansinfo.weekday][index].sans.start_time , temp_sanses[0][sansinfo.weekday][index].sans.end_time)
+        this.setState(()=>({sanses:temp_sanses}))
     }
    
 
     render(){
         return(
-            <PreviewTimetable 
-                onConfirmChange={this.onConfirmChange} 
-                deleteSans={this.deleteSans} 
-                sanses={this.state.sanses}
-                firstRangeValidation={this.validate_first_range}
-                firstRangeError={this.state.first_range_error}
-                secondRangeValidation={this.validate_second_range}
-                secondRangeError={this.state.second_range_error}
+            <div>
+                <Grid centered>
+                    <Grid.Column computer={14}>
+                        <Header dividing textAlign="right">
+                            جدول زمانی پیش فرض
+                            <Header.Subheader>
+                                مطابق با برنامه‌ی خود در جدول زمانی تغییر ایجاد کنید
+                            </Header.Subheader>
+                        </Header>
 
-            />
+                    </Grid.Column>
+                    
+                </Grid>
+
+                <Grid centered>
+
+                    <PreviewTimetable 
+                        onConfirmChange={this.onConfirmChange} 
+                        deleteSans={this.deleteSans} 
+                        sanses={this.state.sanses[0]}
+                        firstRangeValidation={this.validate_first_range}
+                        firstRangeError={this.state.first_range_error}
+                        secondRangeValidation={this.validate_second_range}
+                        secondRangeError={this.state.second_range_error}
+
+                    />
+                </Grid>
+                <Divider hidden section/>
+                <Grid centered>
+                    <Button primary onClick={this.onSubmit}>ایجاد سرویس</Button>            
+                </Grid>
+                <Divider hidden section />
+
+            </div>
+            
         )
     }
 }
@@ -218,7 +133,11 @@ const mapStateToProps = (state) => {
     }
 }
 const mapDispatchToProps = (dispatch) => {
+    return{
+        add_service : (informations, days,business_id) => dispatch(add_service_actions.add_service(informations, days,business_id)),
+        change_panel:(panel_name) => dispatch(change_panel(panel_name))
 
+    }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Preview_timetable_on_addService)
